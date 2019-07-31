b4d9514d5475409840cf7f199dd267b98c48868f
Wano-k <mmarie.laporte@gmail.com>
2019-07-31 13:06:39 +0200

    Updating binary generation for mac

--- binary-mac.sh
+++ binary-mac.sh
@@ -2,16 +2,19 @@
 pathQt="/Users/macuser/Qt"
 versionQt="5.10.0"
 pathRepos="/Users/macuser/Desktop/RPM"
-releaseName="build-Engine-Desktop_Qt_5_10_0_clang_64bit3-Release"
+releaseName="build-RPG-Paper-Maker-Desktop_Qt_5_10_0_clang_64bit3-Release"
 pathRelease="${pathRepos}/RPG-Paper-Maker/Build/Release/EditorApp"
 
 # Build
-cd $pathRelease
-"${pathQt}/${versionQt}/clang_64/bin/qmake" "${pathRepos}/RPG-Paper-Maker/EditorApp/EditorApp.pro" -spec macx-clang CONFIG+=x86_64
+#rm -rf "${pathRepos}/${releaseName}"
+#rm -rf "${pathRepos}/RPG-Paper-Maker/Build"
+cd "${pathRepos}/RPG-Paper-Maker"
+"${pathQt}/${versionQt}/clang_64/bin/qmake" "${pathRepos}/RPG-Paper-Maker/RPG-Paper-Maker.pro" -spec macx-clang CONFIG+=x86_64
 "/usr/bin/make" qmake_all
 "/usr/bin/make"
 
 # Copy to dependencies and push changes
+cd $pathRelease
 rm -rf "${pathRepos}/Dependencies/Engine/osx/RPG-Paper-Maker.app"
 cp -rf RPG-Paper-Maker.app "${pathRepos}/Dependencies/Engine/osx/RPG-Paper-Maker.app"
 cd "${pathRepos}/Dependencies/Engine/osx"
